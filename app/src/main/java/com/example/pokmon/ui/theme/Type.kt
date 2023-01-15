9a9cbcbe61800cdacf55581ce6e12c09e849bd63
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-01-15 15:07:36 +0100

    Intial setup and add some dependencies

--- app/src/main/java/com/example/pokmon/ui/theme/Type.kt
+++ app/src/main/java/com/example/pokmon/ui/theme/Type.kt
@@ -2,17 +2,23 @@ package com.example.pokmon.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.pokmon.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
